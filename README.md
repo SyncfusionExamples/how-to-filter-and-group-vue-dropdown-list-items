baaca2b94d5117b184d957ac9a8cbed9b1bbd39e
Uma Maheswari Chandrabose <39375672+UmaMaheswariChandrabose@users.noreply.github.com>
2019-12-12 16:32:04 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# how-to-filter-and-group-vue-dropdown-list-items
-A quick start Vue project that helps you to learn how to perform Grouping and Filtering with the items of Vue DropdownList of Syncfusion.
+# How to Group and Filter the Syncfusion Vue DropDownList Items
+
+Get a quick overview on how to group and filter items in the Vue Dropdown List. You will learn how to group the Vue Dropdown List items as well as how to filter those items. You will also see different filter types in the Dropdown List, how to apply those filters to items, and how to ignore diacritic characters.  
+
+Documentatio Link: 
+https://ej2.syncfusion.com/vue/documentation/drop-down-list/grouping
+https://ej2.syncfusion.com/vue/documentation/drop-down-list/filtering
+
+
+Demo Link: 
+https://ej2.syncfusion.com/vue/demos/#/material/drop-down-list/grouping-icon.html
+https://ej2.syncfusion.com/vue/demos/#/material/drop-down-list/filtering.html
+
+
+## Project pre-requisites
+Make sure that you have the compatible versions of Node and Vue-Cli in your machine before starting to work on this project.
+
+## How to run this application?
+To run this application, you need to first clone the `how-to-group-and-filter-the-vue-dropdown-list-items` repository and then navigate to its appropriate path where it has been located in your system.
+
+To do so, open the command prompt and run the below commands one after the other.
+
+```
+git clone https://github.com/SyncfusionExamples/how-to-group-and-filter-the-vue-dropdown-list-items dropdownlist-component
+cd dropdownlist-component
+```
+
+## Installing
+Once done with downloading, next you need to install the necessary packages required to run this application locally. The `npm install` command will install all the needed Vue packages into your current project and to do so, run the below command.
+
+```
+npm install
+```
+
+## Running on development server
+Run `npm run serve` command for a dev server. Navigate to `http://localhost:8080/`. The app will automatically reload if you change any of the source files.
+
+## Further help
+
+To get more help on the vue CLI use go check out the [Vue-Cli README](https://github.com/vuejs/vue-cli/blob/master/README.md).
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+      <!-- <div id= "dropdownlist" style="margin:10% 40%; width:250px;">
+        <br>
+        <ejs-dropdownlist popupHeight="200px" popupWidth="250px" 
+        :dataSource='dataItem' :fields='dataFields' 
+        placeholder='Select a Name' :allowFiltering='true'
+        :filtering='onFiltering'>
+        </ejs-dropdownlist>
+      </div> -->
+      <div id= "dropdownlist" style="margin:10% 40%; width:250px;">
+        <br>
+        <ejs-dropdownlist popupHeight="200px" popupWidth="250px" 
+        :dataSource='dataItem' placeholder='Select a Name'
+        :allowFiltering='true' ignoreAccent='true'>
+        </ejs-dropdownlist>
+      </div>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { DropDownListPlugin } from "@syncfusion/ej2-vue-dropdowns";
+Vue.use(DropDownListPlugin);
+//import {Query} from "@syncfusion/ej2-data";
+export default Vue.extend({
+  data: function() {
+    return {
+       dataItem : [
+            'Aeróbics',
+            'Aeróbics en Agua',
+            'Aerografía',
+            'Aeromodelaje',
+            'Águilas',
+            'Ajedrez',
+            'Ala Delta',
+            'Álbumes de Música',
+            'Alusivos',
+            'Análisis de Escritura a Mano'
+        ],
+      // dataItem: [
+      //   { Name: 'Cabbage', Category: 'Leafy and Salad', Id: 'item1' },
+      //   { Name: 'Spinach', Category: 'Leafy and Salad', Id: 'item2' },
+      //   { Name: 'Wheat grass', Category: 'Leafy and Salad', Id: 'item3' },
+      //   { Name: 'Yarrow', Category: 'Leafy and Salad', Id: 'item4' },
+      //   { Name: 'Pumpkins', Category: 'Leafy and Salad', Id: 'item5' },
+      //   { Name: 'Chickpea', Category: 'Beans', Id: 'item6' },
+      //   { Name: 'Green bean', Category: 'Beans', Id: 'item7' },
+      //   { Name: 'Horse gram', Category: 'Beans', Id: 'item8' }
+      //   ],
+      // dataFields: { 
+      //   groupBy: 'Category', 
+      //   text: 'Name', 
+      //   value: 'Id' }
+    }
+  },
+    // methods: {
+    //     onFiltering: function(e) {
+    //         var filterQuery = new Query();
+    //         //frame the query based on search string with filter type.
+    //         filterQuery = (e.text !== '') ? filterQuery.where('Name', 'contains', e.text, true) : filterQuery;
+    //         //pass the filter data source, filter query to updateData method.
+    //         e.updateData(this.dataItem, filterQuery);
+    //     }
+    // }
+});
+
+</script>
+<style>
+@import url(https://cdn.syncfusion.com/ej2/material.css);
+</style>
